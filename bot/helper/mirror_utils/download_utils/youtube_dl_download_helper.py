62d9fa1a990feb98e50f97552406461811d1e287
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-11 07:10:43 -0600

    Update youtube_dl_download_helper.py

--- bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
+++ bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
@@ -2,7 +2,7 @@ from .download_helper import DownloadHelper
 import time
 from yt_dlp import YoutubeDL, DownloadError
 from bot import download_dict_lock, download_dict
-from ..status_utils.yt_dlp_download_status import YoutubeDLDownloadStatus
+from ..status_utils.youtube_dl_download_status import YoutubeDLDownloadStatus
 import logging
 import re
 import threading
