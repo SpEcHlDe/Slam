6a820aaaf5f61af18bab1a2b9dd3388113f3539b
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2022-01-28 00:47:44 -0600

    Initial commit

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -610,9 +610,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirrorbot Search',
-                                 author_name='Slam Mirrorbot',
-                                 author_url='https://github.com/SlamDevs/slam-mirrorbot',
+                                 title = 'AB Mirrorbot Search',
+                                 author_name='AB Mirrorbot',
+                                 author_url='https://github.com/SpEcHlDe/slam',
                                  html_content=content)
         return
 
